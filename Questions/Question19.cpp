9a6a5491733d066efa6907bb4ac4570b3cc6a021
alexendariaa <alexendaria_78_6@proton.me>
2024-07-31 13:05:16 +0000

    add Question19
    modified Question19

--- Questions/Question19.cpp
+++ Questions/Question19.cpp
@@ -2,9 +2,21 @@
 using namespace std;
 //write a funciton to perform linear search
 int search(int arr[],int size,int key){
-    for(int i = 0;i< size ;i++){
-        if (arr[i] == key){
-            cout << arr[i] << endl;
-        }
+    for(int i = 0;i < size ;i++){
+        if (arr[i] == key)
+            return i;
     }
+    return -1;
+}
+int main(){
+    int arr[] = {5,6,8,7,1,3,4};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int key;
+    cout << "Enter the key to perform linear search "<< endl;
+    cin >> key;
+    int result = search(arr,size,key);
+    (result == -1)
+    ? cout << "The key is not present in the the array "
+    : cout << "The key is present at index :" << result << endl;
+    return 0;
 }
